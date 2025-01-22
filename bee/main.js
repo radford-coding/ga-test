6ce2522cd2eb693424103e53e013a74abf5015b3
Matt Radford <quinquex@gmail.com>
2025-01-21 16:39:02 -0800

    added flexbox... not perfectly

--- bee/main.js
+++ bee/main.js
@@ -8,13 +8,13 @@ document.querySelector(".words").innerHTML = guesses;
 
 
 function assignLetters(lttrs) {
-  document.querySelector("#center").innerHTML = lttrs[0];
-  document.querySelector("#top").innerHTML = lttrs[1];
-  document.querySelector("#topright").innerHTML = lttrs[2];
-  document.querySelector("#bottomright").innerHTML = lttrs[3];
-  document.querySelector("#bottom").innerHTML = lttrs[4];
-  document.querySelector("#bottomleft").innerHTML = lttrs[5];
-  document.querySelector("#topleft").innerHTML = lttrs[6];
+  document.querySelector("#centerLetter").innerHTML = lttrs[0];
+  document.querySelector("#topLetter").innerHTML = lttrs[1];
+  document.querySelector("#toprightLetter").innerHTML = lttrs[2];
+  document.querySelector("#bottomrightLetter").innerHTML = lttrs[3];
+  document.querySelector("#bottomLetter").innerHTML = lttrs[4];
+  document.querySelector("#bottomleftLetter").innerHTML = lttrs[5];
+  document.querySelector("#topleftLetter").innerHTML = lttrs[6];
 }
 
 let letters = "FALIGNP";
